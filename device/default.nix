4681d55ebac7f50f7b917ae61eccc49536663f28
AlmiriQ <al.quali.4ef@gmail.com>
2024-04-17 17:06:26 +0700

    Flakes

--- device/default.nix
+++ device/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 
 {
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 	nixpkgs.config.allowUnfree = true;
 	networking.hostName = "aq-${config.aq.qore}";
 }
